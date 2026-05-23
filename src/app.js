16d8d8be67fd95ab5b7cbf316ae9812555c7b2c6
hiwanz <princeb4d@gmail.com>
2026-05-23 23:50:54 +0800

    feat: Frameless window with custom macOS-style title bar controls

--- src/app.js
+++ src/app.js
@@ -1,51 +1,48 @@
-// Modules to control application life and create native browser window
-const { app, BrowserWindow, shell } = require('electron')
+const { app, BrowserWindow, shell, ipcMain } = require('electron')
 const path = require('path')
 
 const createWindow = () => {
-  // Quit when installing, see https://www.electronforge.io/config/makers/squirrel.windows#my-app-is-launching-multiple-times-during-install
   if (require('electron-squirrel-startup')) { return app.quit() }
-  // Create the browser window.
+
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
+    titleBarStyle: 'hidden',
     webPreferences: {
-      devTools: false
+      devTools: false,
+      preload: path.join(__dirname, 'preload.js')
     }
   })
-  // Hide the menu bar
+
   mainWindow.removeMenu()
-  // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'))
 
-  // Open links in external browser
   mainWindow.webContents.setWindowOpenHandler((details) => {
     shell.openExternal(details.url)
   })
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  ipcMain.on('window:minimize', () => mainWindow.minimize())
+  ipcMain.on('window:maximize', () => {
+    mainWindow.isMaximized() ? mainWindow.unmaximize() : mainWindow.maximize()
+  })
+  ipcMain.on('window:close', () => mainWindow.close())
 }
-// Set about panel information
+
 app.setAboutPanelOptions({
   iconPath: 'src/img/icon-64.png',
-  credits: 'Thx for s12v\'s work',
+  credits: "Thx for s12v's work",
   authors: ['hiwanz', 's12v'],
   website: 'https://github.com/hiwanz/hasher-app'
 })
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+
 app.whenReady().then(() => {
   createWindow()
-
   app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
-// Quit when all windows are closed.
+
 app.on('window-all-closed', () => {
   app.quit()
 })
---
+++ src/preload.js
@@ -0,0 +1,7 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  minimize: () => ipcRenderer.send('window:minimize'),
+  maximize: () => ipcRenderer.send('window:maximize'),
+  close:    () => ipcRenderer.send('window:close'),
+})
